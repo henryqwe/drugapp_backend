cdceb9d397f463146af041456e52e6a6d8e85d85
Wojciech Liberda <wojciech.liberda@yougov.com>
2020-08-09 10:38:59 +0200

    Add Dockerfile

--- src/Routing.kt
+++ src/Routing.kt
@@ -1,20 +1,21 @@
 package com.drugapp
 
 import io.ktor.application.call
+import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.ktor.routing.route
-import java.net.http.HttpClient
-import java.util.*
 
-fun Routing.search(client: io.ktor.client.HttpClient) {
+fun Routing.search(client: HttpClient) {
     route("api") {
         get("search") {
             val get = client.get<String>("https://en.wikipedia.org/wiki/Main_Page")
             call.respond(get)
         }
+
+        // test
     }
 
 }
